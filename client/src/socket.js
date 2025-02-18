a92ec845c62ab0064381da2648c568c7b2cfade4
manas patel <patelmanas2003@gmail.com>
2025-02-18 11:54:09 +0530

    Message

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,20 +1,13 @@
-// import { io } from "socket.io-client";
-
-// export const initSocket = async () => {
-//   const options = {
-//     "force new connection": true,
-//     reconnectionAttempt: "Infinity",
-//     timeout: 10000,
-//     transports: ["websocket"],
-//   };
-//   return io(process.env.REACT_APP_BACKEND_URL, options);
-// };
-
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_BACKEND_URL, {
+export const initSocket = async () => {
+  const options = {
+    "force new connection": true,
+    reconnectionAttempt: "Infinity",
+    timeout: 10000,
     transports: ["websocket"],
-    withCredentials: true
-});
+  };
+  return io(process.env.REACT_APP_BACKEND_URL, options);
+};
 
-export default socket;
+
